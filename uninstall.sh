8ad3a5c61d703d24afa7635ce84db1be7391d060
CJackYang <cjackyang@qq.com>
2018-05-10 14:22:30 +0800

    tmp

--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,10 @@ set -e
 echo "uninstall appifi && clean volumes"
 
 #umount blocks
-umount /dev/sdb
-umount /dev/sdc
+umount /dev/sdb || echo "umount /dev/sdb failed"
+umount /dev/sdc || echo "umount /dev/sdc failed"
 
+echo "clean boundVolume"
 rm -r /run/phicomm/
 
 rm -r /etc/phicomm/
