7f2e0994fdb093de9766ba0e3944a34694b2b33a
CJackYang <ijackyang@sina.cn>
2018-06-29 15:54:18 +0800

    tmp

--- src/router/app.js
+++ src/router/app.js
@@ -70,7 +70,7 @@ module.exports = (auth, model) => {
     res.status(200).json({
       deviceSN: deviceInfo.deviceSN,
       deviceModel: deviceInfo.deviceModel,
-      mac: deviceInfo.net && device.net.mac,
+      mac: deviceInfo.net && deviceInfo.net.mac,
       swVersion: deviceInfo.softwareVersion,
       hwVersion: deviceInfo.hardwareVersion,
       boundUser: model.account.user ? { phicommUserId: model.account.user.phicommUserId} : null,
